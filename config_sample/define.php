2f324dff84a48ba148f60bea694e3a4d7ac40955
vansteki <vansteki.tw@gmail.com>
2013-06-23 18:21:26 +0800

    Create define.php
    define path

---
+++ config_sample/define.php
@@ -0,0 +1,17 @@
+<?php
+    define('ROOT', dirname(dirname(__FILE__))); // /var/www/projectName
+	define('HOST_URL', '');
+	define('FQDN', '');
+	define('DB_CONF_PATH', '');
+	define('DB_UTIL', '');
+	define('UTIL', '');
+	define('INC', '');
+	define('MODEL', '');
+	define('VIEW', '');
+	define('SESSION_MAX_TIME', 0);
+	
+	$ROOT =  ROOT;
+	$HOST_URL = HOST_URL;
+	$FQDN = FQDN; 		
+	$DB_CONF_PATH = DB_CONF_PATH;	
+?>
