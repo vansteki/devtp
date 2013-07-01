5b8dc84185b2919e3da87268202bd4eedf3be0ac
vansteki <vansteki.tw@gmail.com>
2013-07-02 02:37:30 +0800

    Update define.php

--- config_sample/define.php
+++ config_sample/define.php
@@ -1,23 +1,22 @@
 <?php
-    	define('ROOT', dirname(dirname(__FILE__))); // /var/www/projectName
+	define('ROOT', dirname(dirname(__FILE__))); // /var/www/projectName
 	define('HOST_URL', '');
 	define('FQDN', '');
 	define('DB_CONF_PATH', '');
-	define('DB_UTIL', '');
-	define('UTIL', '');
-	define('INC', '');
-	define('MODEL', '');
-	define('VIEW', '');
-	define('CTRL', '');
-	define('SESSION_MAX_TIME', 0);
+	define('DB_UTIL', ROOT.'/include/db_util.php');
+	define('UTIL', ROOT.'/include/util.php');
+	define('INC',ROOT.'/include');
+	define('MODEL', ROOT.'/model');
+	define('VIEW', ROOT.'/view');
+	define('CTRL', ROOT.'/controller');
 	define('VIEW_URL', HOST_URL.'/view');
 	define('CTRL_URL', HOST_URL.'/controller');
 	define('ADMIN', ROOT.'/model/admin.php');
-
+	define('SESSION_MAX_TIME', 0);
 	
 	$ROOT =  ROOT;
+	$DB_CONF_PATH = DB_CONF_PATH;
 	$HOST_URL = HOST_URL;
 	$FQDN = FQDN; 		
-	$DB_CONF_PATH = DB_CONF_PATH;
-	$CTRL = CTRL;
+	$CTRL = CTRL;	
 ?>
